e73bb12376625f5ad778248ed4c1b3c4aa4d531d
mtal <elon.chiu@tpinformation.com.tw>
2018-09-03 19:50:12 +0800

    加上catchError

--- src/app/mgt02/mgt02.component.ts
+++ src/app/mgt02/mgt02.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
-import { map } from "rxjs/operators";
+import { Http, Response } from '@angular/http';
+import { map, catchError } from "rxjs/operators";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-mgt02',
@@ -14,7 +15,23 @@ export class Mgt02Component {
 
   ngOnInit() {
     let url = '/opendata/datalist/apiAccess?scope=resourceAquire&rid=0b544701-fb47-4fa9-90f1-15b1987da0f5';
-    this.data = this.http.get(url).pipe(map(x => x.json()));
+    // this.data = this.http.get(url).pipe(map(x => x.json()));
+    this.data = this.http.get(url)
+      .pipe(
+        map((response: Response) => response.json()),
+        catchError(this._errorhandler)
+      )
   }
 
+  getData() {
+    // return this.http.get('http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=0b544701-fb47-4fa9-90f1-15b1987da0f5')
+    //   .pipe(
+    //     map((response: Response) => response.json()),
+    //     catchError(this._errorhandler)
+    //   )
+  }
+  _errorhandler(error: Response) {
+    console.log(error);
+    return Observable.throw(error || 'server Error');
+  }
 }
